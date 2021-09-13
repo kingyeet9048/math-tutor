3cac29ad201625144a5b2bfed67400f1a2b0c230
TheMinds3t <42127046+TheMinds3t@users.noreply.github.com>
2021-09-13 18:42:12 -0500

    added getAccountInfo.php
    Utilized session variables
    Added getAccountInfo.php, takes starid (currently hardcoded) and returns f&l name, role and last access timestamp
    Closes #21

--- php/checkCreds.php
+++ php/checkCreds.php
@@ -8,6 +8,18 @@ if ($conn->connect_error) {
     die("Cannot connect to MySQL server to verify credentials. Please try again later.<br>");
 }
 
+session_start();
+
+// if (isset($_SESSION))
+// {
+//     session_unset();
+// } if we need to clear session cache on verification, this is the way to do it
+
+$_SESSION["UEMAIL"] = $email; //store email and password in session variables
+$_SESSION["UPASSWORD"] = $password;
+$_SESSION["DBUN"] = "root";
+$_SESSION["DBPW"] = "root";
+
 // prepare and bind
 $stmt = $conn->prepare("SELECT LGN.password AS 'password', INF.email AS 'email' from mathtutor.login AS LGN INNER JOIN mathtutor.info AS INF ON INF.email = ? AND LGN.password = ?");
 $stmt->bind_param("ss", $email, $password);
@@ -17,7 +29,7 @@ $stmt->execute();
 $result = $stmt->get_result();
 $row = $result->fetch_assoc();
 
-if($row != null) //No login info found
+if($row != null) //login info found
 {
     echo "success";
 }
