96efcc5890549c75bcbc17da552e632b39b5c796
TheMinds3t <42127046+TheMinds3t@users.noreply.github.com>
2021-09-13 19:53:45 -0500

    Added dbAssist.php
    dbAssist.php allows for controlling database credentials and location (think, remote vs local database server). Super barebones, feel free to modify it's appearance but it's meant to be inaccessible.
    Included php/setSessionTimeout.php. Not currently used, but testing some things with it.

--- index.php
+++ index.php
@@ -15,12 +15,52 @@
             <input type="submit">
         </form>
 
+<<<<<<< Updated upstream
         <?php 
             if (isset($_GET["error"]))
             {
                 echo "<br>Error code ".htmlspecialchars($_GET["error"]);
             }
         ?>
+=======
+    <div id="id01" class="modal">
+      
+      <form class="modal-content animate" action="php/checkCreds.php" method="post">
+        <div class="imgcontainer">
+          <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+          <img src="ELE.png" alt="Avatar" class="avatar">
+        </div>
+
+        <div class="container">
+          <label for="uname"><b>Username</b></label>
+          <input type="text" placeholder="Enter Email" name="email" required>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="password" required>
+            
+          <button type="submit">Login</button>
+          <label>
+            <input type="checkbox" checked="checked" name="remember"> Remember me
+          </label>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+          <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+          <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+      </form>
+      <?php 
+        if (isset($_GET["error"]))
+        {
+            echo "<br>Error code ".htmlspecialchars($_GET["error"]);
+        }
+      ?>
+      <!-- This is here since it's for our development purposes. It can be commented, just wanted to include the link. ~ Ashton -->
+      <br><a href="dbAssist.php">Goto database assistance page</a>
+    </div>
+  </body>
+</html>
+>>>>>>> Stashed changes
 
     </body>
 </html> 
