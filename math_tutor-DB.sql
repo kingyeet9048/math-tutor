09890eb0022170ad3e7e4edc94a3b21b92ef85f2
nameer1811 <nameer.fahad@gmail.com>
2021-09-14 23:33:47 -0500

    Added stored procedures for signup and forgot password

--- math_tutor-DB.sql
+++ math_tutor-DB.sql
@@ -29,7 +29,7 @@ CREATE TABLE `questions` (
     `ID` INT PRIMARY KEY AUTO_INCREMENT,
     `courseID` INT,
     `starID` VARCHAR(8),
-    `questionNumber` INT AUTO_INCREMENT,
+    `questionNumber` INT,
     `questionType` VARCHAR(255),
     `isOverride` BOOLEAN
 );
@@ -39,3 +39,37 @@ ALTER TABLE `login` ADD FOREIGN KEY (`starID`) REFERENCES `info` (`starID`);
 ALTER TABLE `courses` ADD FOREIGN KEY (`starID`) REFERENCES `info` (`starID`);
 ALTER TABLE `questions` ADD FOREIGN KEY (`starID`) REFERENCES `info` (`starID`);
 ALTER TABLE `questions` ADD FOREIGN KEY (`courseID`) REFERENCES `courses` (`ID`);
+
+DELIMITER $$
+CREATE PROCEDURE `signUp`(
+IN star_ID VARCHAR(8), 
+IN lName VARCHAR (255), 
+IN fName VARCHAR (255), 
+IN role VARCHAR (14), 
+IN uName VARCHAR (255), 
+IN pWord VARCHAR(255)
+)
+BEGIN
+
+    INSERT INTO mathTutor.info (starID, lastName, firstName, role )
+    VALUES (star_ID, lName, fName, role);
+
+    INSERT INTO mathTutor.login (starID, userName, password)
+    VALUES (star_ID, uName, pWord);
+    
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `forgotPassword`(
+IN star_ID VARCHAR(8), 
+IN pWord VARCHAR(255)
+)
+BEGIN
+
+    UPDATE mathTutor.login
+    SET password = pWord 
+    WHERE starID = star_ID;
+    
+END$$
+DELIMITER ;
