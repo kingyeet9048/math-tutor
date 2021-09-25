69e3a00fb99432019a37de34b9f44d33de8f49c9
nameer1811 <nameer.fahad@gmail.com>
2021-09-24 19:24:13 -0500

    Created 3 stored procedures to check enrolled, deleteCourse, and  check student progress

--- math_tutor-DB.sql
+++ math_tutor-DB.sql
@@ -4,15 +4,26 @@ USE mathTutor;
 DROP TABLE IF EXISTS records;
 DROP TABLE IF EXISTS questions ;
 DROP TABLE IF EXISTS courses ;
+DROP TABLE IF EXISTS studentInfo ;
+DROP TABLE IF EXISTS teacherInfo ;
 DROP TABLE IF EXISTS login ;
-DROP TABLE IF EXISTS info ;
 DROP PROCEDURE IF EXISTS signUp ;
 DROP PROCEDURE IF EXISTS forgotPassword ;
+DROP PROCEDURE IF EXISTS deleteCourse ;
+DROP PROCEDURE IF EXISTS getStudentProgress ;
+DROP PROCEDURE IF EXISTS getEnrolled ;
 
 
+CREATE TABLE `studentInfo` (
+    `studentStarID` VARCHAR(8) PRIMARY KEY UNIQUE,
+    `lastName` VARCHAR(255),
+    `firstName` VARCHAR(255),
+    `courseID` VARCHAR(255),
+    `role` VARCHAR(7)
+);
 
-CREATE TABLE `info` (
-    `starID` VARCHAR(8) PRIMARY KEY UNIQUE,
+CREATE TABLE `teacherInfo` (
+    `teacherStarID` VARCHAR(8) PRIMARY KEY UNIQUE,
     `lastName` VARCHAR(255),
     `firstName` VARCHAR(255),
     `role` VARCHAR(7)
@@ -54,6 +65,7 @@ ALTER TABLE `questions` ADD FOREIGN KEY (`courseID`) REFERENCES `courses` (`ID`)
 ALTER TABLE `records` ADD FOREIGN KEY (`questionID`) REFERENCES `questions` (`ID`);
 ALTER TABLE `records` ADD FOREIGN KEY (`studentStarID`) REFERENCES `info` (`starID`);
 ALTER TABLE `records` ADD FOREIGN KEY (`courseID`) REFERENCES `courses` (`ID`);
+ALTER TABLE `info` ADD FOREIGN KEY (`courseID`) REFERENCES `courses` (`ID`);
 
 DELIMITER $$
 CREATE PROCEDURE `signUp`(
@@ -88,3 +100,54 @@ BEGIN
     
 END$$
 DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `deleteCourse`(
+IN cName VARCHAR(255)
+)
+BEGIN
+
+	SET @courseID = (SELECT ID FROM mathTutor.courses WHERE courseName = cName);
+    DELETE FROM mathTutor.records WHERE courseID = courseID;
+    DELETE FROM mathTutor.questions WHERE courseID = courseID;
+    DELETE FROM mathTutor.courses WHERE courseID = courseID;
+    
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getStudentProgress`(
+IN ssID VARCHAR(8)
+)
+BEGIN
+
+	SET @questionID = (SELECT COUNT(*) FROM mathTutor.courses WHERE studentStarID = ssID);
+	SET @cName = (SELECT courseName FROM mathTutor.courses WHERE studentStarID = ssID);
+    SET @recordID = (SELECT COUNT(*) FROM mathTutor.records WHERE studentStarID = ssID);
+    
+    SET @percentComplete = (@questionID/@recordID) * 100;
+    
+    IF (@cName LIKE '' OR @recordID LIKE 0) THEN
+		SELECT '';
+	ELSE
+		SELECT @percentComplete;
+	END IF;
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE `getEnrolled`(
+IN tsID VARCHAR(8)
+)
+BEGIN
+
+	SET @cID = (SELECT ID FROM mathTutor.courses WHERE teacherStarID = tsID);
+	SET @firstName = (SELECT firstName FROM mathTutor.info WHERE starID = (SELECT studentStarID FROM mathTutor.questions WHERE courseID = @cID));
+	SET @lastName = (SELECT lastName FROM mathTutor.info WHERE starID = (SELECT studentStarID FROM mathTutor.questions WHERE courseID = @cID));
+	SET @correctQ = (SELECT COUNT(*) FROM mathTutor.records WHERE studentStarID = (SELECT studentStarID FROM mathTutor.records WHERE courseID = @cID)) ;
+    SELECT @cID, @firstName, @lastName, @correctQ;
+
+END$$
+DELIMITER ;
+
