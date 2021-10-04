bf4c09e9bc57e799794bdf40d67f012ec3e1ab4b
TheMinds3t <42127046+TheMinds3t@users.noreply.github.com>
2021-10-03 22:04:09 -0500

    Updated a few scripts to work correctly with new db schema
    fixed checkCreds, connectToDB, isStarIDTeacher, and updatePassword

--- php/isStarIDTeacher.php
+++ php/isStarIDTeacher.php
@@ -1,32 +1,48 @@
 <?php 
-
+session_start();
 include("helper/connectToDB.php");
 $conn = connectToDB();
 $starID = $_SESSION["USTARID"];
 
 $rawdata = file_get_contents("php://input");
 $decodedData = json_decode($rawdata);
-//getting the raw sha256 output
-$starID = $decodedData->starID;
+$returnState = new stdClass();
 
-if(!isset($starID))
+if(!isset($decodedData->starID))
 {
-    $starID = $_POST["starID"];
+    $returnState->error = "StarID was not found. Try passing 'starID' as a parameter when calling this script.";
+}
+else
+{
+    //getting the raw sha256 output
+    $starID = $decodedData->starID;
+    if(!isset($starID))
+    {
+        $starID = $_SESSION["USTARID"];
+    }
+    
+    if($starID != null)
+    {
+        echo $starID;
+        // prepare and bind
+        $stmt = $conn->prepare("SELECT TCH.teacherStarID FROM mathtutor.teacherinfo AS INF WHERE INF.teacherStarID = ?;");
+        $stmt->bind_param("s", $starID);
+    
+        //execute and receive query results
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+        $returnState -> $row != null;
+    
+        $stmt->close();
+        $conn->close();
+    }
+    else
+    {
+        $returnState->error = "StarID was not found. Try passing 'starID' as a parameter when calling this script.";
+    }
 }
 
-// prepare and bind
-$stmt = $conn->prepare("SELECT TCH.teacherStarID FROM mathtutor.teacherinfo AS INF WHERE INF.teacherStarID = ?;");
-$stmt->bind_param("s", $starID);
-
-//execute and receive query results
-$stmt->execute();
-$result = $stmt->get_result();
-$row = $result->fetch_assoc();
-$returnState -> $row != null;
-
 echo json_encode($returnState);
-
-$stmt->close();
-$conn->close();
 
 ?>
